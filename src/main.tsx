844f47f59a683de21195ccf7f74640ad218a3f30
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-06-12 17:09:20 +0800

    add login and signup pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App.tsx';        
 import About from './pages/About.tsx';
 import Contact from './pages/Contact.tsx';
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
